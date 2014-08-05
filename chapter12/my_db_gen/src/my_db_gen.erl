67e339bd5a11d1d3cd8036d6d7e66409d9ff5fcf
Shane Charles <shanecharles@burningicesolutions.com>
2014-08-05 15:35:11 -0500

    can start and stop my_db_gen server

---
+++ chapter12/my_db_gen/src/my_db_gen.erl
@@ -0,0 +1,169 @@
+%%%-------------------------------------------------------------------
+%%% @author Shane Charles <shanecharles@burningicesolutions.com>
+%%% @copyright (C) 2014, Burning Ice Solutions Inc.
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2014-08-05 14:18:15.945007
+%%%-------------------------------------------------------------------
+-module(my_db_gen).
+
+-behaviour(gen_server).
+
+%% API
+-export([start/0, stop/0]). %%, write/2, delete/1, read/1, match/1]).
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+%%         handle_info/2,
+         terminate/2
+%%         code_change/3
+        ]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Writes the key/value pair to the database
+%%
+%% @spec write(Key, Element) -> ok
+%% @end
+%%--------------------------------------------------------------------
+write(Key, Element) ->
+    gen_server:call({write, Key, Element}).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Stops the server
+%%
+%% @spec stop() -> ok
+%% @end
+%%--------------------------------------------------------------------
+stop() ->
+    exit(whereis(?SERVER), shutdown),
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start() -> ok
+%% @end
+%%--------------------------------------------------------------------
+start() ->
+    {ok, _Pid} = gen_server:start({local, ?SERVER}, ?MODULE, [], []),
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+        gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+        {ok, db:new()}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(stop, _From, State) ->
+        
+        Reply = ok,
+        {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+        {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+%%handle_info(_Info, State) ->
+%%        {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, State) ->
+        db:destroy(State),
+        ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+%%code_change(_OldVsn, State, _Extra) ->
+%%        {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
+
+
